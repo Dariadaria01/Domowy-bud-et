1b9e2f80d02e22aaafeb96db281f8419e3faf9ce
Dariadaria01 <dariaxdee1@o2.pl>
2024-07-16 21:13:57 +0200

    Domowy budzet

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const incomeForm = document.getElementById('incomeForm');
 const incomeList = document.getElementById('incomeList');
+const expenseForm = document.getElementById('expenseForm');
+const expenseList = document.getElementById('expenseList');
+const totalText = document.getElementById('total');
+const expenseData = [];
 const incomeData = [];
+
+const calculateTotal = () => {
+  const totalExpense = expenseData.reduce((prevValue, currValue) => {
+    return prevValue + currValue.amount;
+  }, 0);
+  const totalIncome = incomeData.reduce((prevValue, currValue) => {
+    return prevValue + currValue.amount;
+  }, 0);
+  const total = totalIncome - totalExpense;
+  if (total > 0) {
+    totalText.innerHTML = `Możesz jeszcze wydać ${total} złotych`;
+  }
+  if (total < 0) {
+    totalText.innerHTML = `Bilans jest ujemny. Jesteś na minusie ${total} złotych`;
+  }
+  if (total === 0) {
+    totalText.innerHTML = `Bilans wynosi zero`;
+  }
+};
 
 const renderIncomeList = () => {
   incomeList.innerHTML = '';
@@ -13,11 +36,14 @@ const renderIncomeList = () => {
     incomeList.appendChild(li);
 
     removeButton.addEventListener('click', () => {
-      const indexToRemove = incomeData.findIndex((item) => item.id === income.id);
+      const indexToRemove = incomeData.findIndex(
+        (item) => item.id === income.id
+      );
       incomeData.splice(indexToRemove, 1);
       renderIncomeList();
     });
   });
+  calculateTotal();
 };
 
 incomeForm.addEventListener('submit', (event) => {
@@ -31,4 +57,80 @@ incomeForm.addEventListener('submit', (event) => {
   };
   incomeData.push(newIncome);
   renderIncomeList();
+});
+
+const renderExpenseList = () => {
+  expenseList.innerHTML = '';
+  expenseData.map((expense) => {
+    const li = document.createElement('li');
+    li.textContent = `${expense.title} - ${expense.amount}`;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'usuń';
+    li.appendChild(removeButton);
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edytuj';
+    li.appendChild(editButton);
+    expenseList.appendChild(li);
+
+    removeButton.addEventListener('click', () => {
+      const indexToRemove = expenseData.findIndex(
+        (item) => item.id === expense.id
+      );
+      expenseData.splice(indexToRemove, 1);
+      renderExpenseList();
+    });
+
+    editButton.addEventListener('click', () => {
+      li.innerHTML = '';
+      const form = document.createElement('form');
+      const inputTitle = document.createElement('input');
+      inputTitle.name = 'expenseTitle';
+      inputTitle.required = true;
+      inputTitle.value = expense.title;
+      const inputAmount = document.createElement('input');
+      inputAmount.name = 'expenseAmount';
+      inputAmount.type = 'number';
+      inputAmount.required = true;
+      inputAmount.min = 0.01;
+      inputAmount.step = 0.01;
+      inputAmount.value = expense.amount;
+      const saveButton = document.createElement('button');
+      const cancelButton = document.createElement('button');
+      saveButton.textContent = 'Zapisz';
+      cancelButton.textContent = 'Anuluj';
+      cancelButton.type = 'reset';
+      form.appendChild(inputTitle);
+      form.appendChild(inputAmount);
+      form.appendChild(saveButton);
+      form.appendChild(cancelButton);
+
+      li.appendChild(form);
+
+      cancelButton.addEventListener('click', () => {
+        renderExpenseList();
+      });
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const expenseTitle = event.target.expenseTitle.value;
+        const expenseAmount = event.target.expenseAmount.valueAsNumber;
+        expense.title = expenseTitle;
+        expense.amount = expenseAmount;
+        renderExpenseList();
+      });
+    });
+  });
+  calculateTotal();
+};
+
+expenseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const expenseTitle = event.target.expenseTitle.value;
+  const expenseAmount = event.target.expenseAmount.valueAsNumber;
+  const newExpense = {
+    title: expenseTitle,
+    amount: expenseAmount,
+    id: crypto.randomUUID(),
+  };
+  expenseData.push(newExpense);
+  renderExpenseList();
 });
